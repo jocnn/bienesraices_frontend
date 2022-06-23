29baf041b3d404622c691bcb8f2012e96003155a
jocnn <ijann_@outlook.com>
2022-06-23 07:59:16 -0300

    usando helmet para importar en el header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,22 @@
 import * as React from "react"
+import { Helmet } from "react-helmet"
 
 const Layout = ({children}) => {
   return (
     <>
+      <Helmet>
+        <link 
+          rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" 
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" 
+          crossOrigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:wght@400;700&display=swap" 
+          rel="stylesheet" /> 
+        <title>Bienes Raices Gatsby</title>
+      </Helmet>
+
       {children}
     </>
   )
