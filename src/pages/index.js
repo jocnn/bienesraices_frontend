6953c7f2845a1f1e45e0895edd24708835de32ec
jocnn <ijann_@outlook.com>
2022-06-23 07:48:14 -0300

    importando el layout en index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 
+import Layout from "../components/layout"
+
 const Index = () => {
   return (
-    <>
+    <Layout>
       <h1>Index</h1>
-    </>
+    </Layout>
   )
 }
 
