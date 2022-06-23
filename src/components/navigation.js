83893e63bcb5f2dc8173565e62f40ef1deb75090
jocnn <ijann_@outlook.com>
2022-06-23 09:18:06 -0300

    agregando estilos a la navegación

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,51 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import styled from "@emotion/styled"
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 3rem;
+
+  @media (min-width: 768px) {
+    padding: 0;
+    flex-direction: row;
+  }
+`
+
+const NavLink = styled(Link)`
+  color: #fff;
+  font-weight: 700;
+  font-family: var(--fuentePrimaria);
+  padding: .5rem;
+  text-decoration: none;
+  margin-right: 1rem;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+  &.pagina-actual {
+
+    border-bottom: 2px solid #fff;
+  }
+`
 
 const Navigation = () => {
   return (
-    <nav>
-      <Link to={'/'}>Inicio</Link>
-      <Link to={'/about'}>Nosotros</Link>
-      <Link to={'/properties'}>Propiedades</Link>
-    </nav>
+    <Nav>
+      <NavLink
+        to={'/'}
+        activeClassName='pagina-actual'
+      >Inicio</NavLink>
+      <NavLink
+        to={'/about'}
+        activeClassName='pagina-actual'
+      >Nosotros</NavLink>
+      <NavLink
+        to={'/properties'}
+        activeClassName='pagina-actual'
+      >Propiedades</NavLink>
+    </Nav>
   )
 }
 
