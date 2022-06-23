9982015e3c117101a02a24b310867351f8dbb895
jocnn <ijann_@outlook.com>
2022-06-23 08:53:24 -0300

    agregando estilos al header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,30 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { css } from "@emotion/react"
 
 import Navigation from "./navigation"
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header
+      css={css`
+        background-color: #0D2838;
+        padding: 1rem;
+      `}
+    >
+      <div
+        css={css`
+          max-width: 120rem;
+          margin: 0 auto;
+          text-align: center;
+
+          @media (min-width: 768px) {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
+        `}
+      >
         <Link to={'/'}>Bienes Raíces</Link>
 
         <Navigation />
