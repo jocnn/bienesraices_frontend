8e049c88f22ab645f72d89634130f4654b1e7ec5
jocnn <ijann_@outlook.com>
2022-06-23 08:45:37 -0300

    se creo página properties

---
+++ src/pages/properties.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const Properties = () => {
+  return (
+    <Layout>
+      <h1>Properties</h1>
+    </Layout>
+  )
+}
+
+export default Properties
