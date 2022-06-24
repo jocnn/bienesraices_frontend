ec2d128574becfea28519f78ba12ca418ffee374
jocnn <ijann_@outlook.com>
2022-06-24 08:37:16 -0300

    consultando datos de strapi con graphql

--- src/hooks/useIndex.js
+++ src/hooks/useIndex.js
@@ -1,21 +1,33 @@
-import * as React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
 const useIndex = () => {
 
   const result = useStaticQuery(graphql`
-    query {
-      allStrapiPagina(filter: {titulo: {eq: "Inicio"}}) {
-        nodes {
-          titulo
-          contenido
-          id
+  query {
+    allStrapiPagina(filter: {titulo: {eq: "Inicio"}}) {
+      nodes {
+        titulo
+        contenido
+        imagen {
+          localFile {
+            childrenImageSharp {
+              gatsbyImageData
+            }
+          }
         }
       }
     }
+  }
   `)
 
-  return(result.allStrapiPagina.nodes[0])
+  //console.log('result: ', result)
+
+  return result.allStrapiPagina.nodes.map( inicio => ({
+    id: inicio.id,
+    titulo: inicio.titulo,
+    contenido: inicio.contenido,
+    imagen: inicio.imagen.localFile.childrenImageSharp[0].gatsbyImageData
+  }))
   
 }
 
