5abd8d6864ae115c96d0a3afdd999a470adcef65
jocnn <ijann_@outlook.com>
2022-06-24 08:37:56 -0300

    mostrando datos extraidos desde strapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,42 @@
 import * as React from "react"
+import { css } from "@emotion/react"
+import styled from "@emotion/styled"
+import { GatsbyImage } from "gatsby-plugin-image"
+
 import useIndex from "../hooks/useIndex"
 
 import Layout from "../components/layout"
 
+const ImageBackground = styled(GatsbyImage)`
+  height: 600px;
+`
+
 const Index = () => {
 
   const index = useIndex()
-
-  console.log(index)
-
+  //console.log("🚀 ~ file: index.js ~ line 17 ~ Index ~ index", index)
+  
+  const { titulo, contenido, imagen } = index[0]
+  
   return (
     <Layout>
-      <h1>Index</h1>
+      <ImageBackground
+        image={imagen}
+        alt="imagen de página"
+      ></ImageBackground>
+      <main>
+        <div css={css`
+          max-width: 80rem;
+          margin: 0 auto;
+        `}>
+          <h1>{titulo}</h1>
+          <p
+            css={css`
+              text-align: center;
+            `}
+          >{contenido}</p>
+        </div>
+      </main>
     </Layout>
   )
 }
