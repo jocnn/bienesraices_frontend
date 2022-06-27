dfdcc6e014eb2f43308171ddf7b41c3bb85d4935
jocnn <ijann_@outlook.com>
2022-06-27 19:48:02 -0300

    custom hook que lista las propiedades con graphql

---
+++ src/hooks/useProperties.js
@@ -0,0 +1,59 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+const useProperties = () => {
+
+  const result = useStaticQuery(graphql`
+    query {
+      allStrapiPropiedad {
+        nodes {
+          id
+          habitaciones
+          estacionamiento
+          nombre
+          precio
+          descripcion {
+            data {
+              internal {
+                content
+              }
+            }
+          }
+          categoria {
+            nombre
+          }
+          banhos
+          agente {
+            email
+            nombre
+            telefono
+          }
+          imagen {
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+                fluid(maxWidth: 600, maxHeight: 400) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return result.allStrapiPropiedad.nodes.map(propiedad => ({
+    id: propiedad.id,
+    habitaciones: propiedad.habitacion,
+    estacionamiento: propiedad.estacionamiento,
+    nombre: propiedad.nombre,
+    precio: propiedad.precio,
+    descripcion: propiedad.descripcion.data.internal.content,
+    categoria: propiedad.categoria.nombre,
+    banhos: propiedad.banhos,
+    agente: propiedad.agente,
+    imagen: propiedad.imagen.localFile.childImageSharp.gatsbyImageData
+  }))
+}
+
+export default useProperties
