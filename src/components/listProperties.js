8c8f77992befa40a21acc3cabd219be7c194a7d4
jocnn <ijann_@outlook.com>
2022-06-27 19:48:45 -0300

    componente para listar las  propiedades

---
+++ src/components/listProperties.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { css } from "@emotion/react"
+
+
+const ListadoPropiedades = () => {
+  return (
+    <>
+      <h1
+        css={css`
+          margin-top: -5rem;
+          margin-bottom: 5rem;
+        `}
+      >ListadoPropiedades</h1>
+    </>
+  )
+}
+
+export default ListadoPropiedades
