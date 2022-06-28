0b3cb1852fa8810b32813e481ba64ab9ac4112f3
jocnn <ijann_@outlook.com>
2022-06-27 21:39:20 -0300

    se agrego estilos

--- src/components/iconos.js
+++ src/components/iconos.js
@@ -1,5 +1,21 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import styled from "@emotion/styled"
+
+const ListadoIconos = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  flex: 1;
+  max-width: 50rem;
+  margin: 3px auto 0 auto;
+
+  li {
+    display: flex;
+    img {
+      margin-right: 1rem;
+    }
+  }
+`
 
 const Iconos = ({ banhos, estacionamiento, habitaciones }) => {
   
@@ -21,7 +37,7 @@ const Iconos = ({ banhos, estacionamiento, habitaciones }) => {
   // console.log("🚀 ~ file: iconos.js ~ line 26 ~ Iconos ~ imgIcons", imgIcons)
   
   return (
-    <ul>
+    <ListadoIconos>
       <li>
         <img src={imgIcons[2].node.publicURL} alt="icono de banhos" />
         <p>{banhos}</p>
@@ -34,7 +50,7 @@ const Iconos = ({ banhos, estacionamiento, habitaciones }) => {
         <img src={imgIcons[0].node.publicURL} alt="icono de habitaciones" />
         {habitaciones}
       </li>
-    </ul>
+    </ListadoIconos>
   )
 }
 
