ba0167837d476478eceec3714b34a8de2c02951f
jocnn <ijann_@outlook.com>
2022-06-29 08:09:36 -0300

    fix, formateo de codigo

--- src/hooks/useIndex.js
+++ src/hooks/useIndex.js
@@ -1,37 +1,41 @@
 import { graphql, useStaticQuery } from "gatsby"
 
 const useIndex = () => {
-
   const result = useStaticQuery(graphql`
-  query {
-    allStrapiPagina(filter: {titulo: {eq: "Inicio"}}) {
-      nodes {
-        titulo
-        contenido
-        imagen {
-          localFile {
-            childImageSharp {
-              gatsbyImageData(
-                transformOptions: {duotone: {highlight: "#222222", shadow: "#192550", opacity: 30}}
-                sizes: "1200px"
-              )
+    query {
+      allStrapiPagina(filter: { titulo: { eq: "Inicio" } }) {
+        nodes {
+          titulo
+          contenido
+          imagen {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(
+                  transformOptions: {
+                    duotone: {
+                      highlight: "#222222"
+                      shadow: "#192550"
+                      opacity: 30
+                    }
+                  }
+                  sizes: "1200px"
+                )
+              }
             }
           }
         }
       }
     }
-  }
   `)
 
   //console.log('result: ', result)
 
-  return result.allStrapiPagina.nodes.map( inicio => ({
+  return result.allStrapiPagina.nodes.map(inicio => ({
     id: inicio.id,
     titulo: inicio.titulo,
     contenido: inicio.contenido,
-    imagen: inicio.imagen.localFile.childImageSharp.gatsbyImageData
+    imagen: inicio.imagen.localFile.childImageSharp.gatsbyImageData,
   }))
-  
 }
 
 export default useIndex
