2b2332d8f5ada0c76574c5809d4a7c1d59bb5707
jocnn <ijann_@outlook.com>
2022-06-30 10:46:06 -0300

    creando páginas dinámicas para propiedades

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,34 @@
-// exports.createPages = async ({ actions }) => {
-//   const { createPage } = actions
-//   createPage({
-//     path: "/using-dsg",
-//     component: require.resolve("./src/templates/using-dsg.js"),
-//     context: {},
-//     defer: true,
-//   })
-// }
+const urlSlug = require("url-slug")
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allStrapiPropiedad {
+        nodes {
+          id
+          nombre
+        }
+      }
+    }
+  `)
+  // console.log("🚀 ~ file: gatsby-node.js ~ line 20 ~ exports.createPages= ~ result", JSON.stringify(result))
+
+  if (result.error) {
+    reporter.panic("No hubo resultados", result.error)
+  }
+
+  const propiedades = result.data.allStrapiPropiedad.nodes
+  // console.log("🚀 ~ file: gatsby-node.js ~ line 27 ~ exports.createPages= ~ propiedades", JSON.stringify(propiedades))
+
+  propiedades.forEach(propiedad => {
+    // console.log("🚀 ~ file: gatsby-node.js ~ line 30 ~ exports.createPages= ~ propiedad", propiedad.id)
+    actions.createPage({
+      path: urlSlug(propiedad.nombre),
+      component: require.resolve('./src/components/properties.js'),
+      context: {
+        id: propiedad.id
+      }
+    })
+  })
+
+}
