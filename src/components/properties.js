f990f439a2783696e76f90b37b301a996903267c
jocnn <ijann_@outlook.com>
2022-06-30 11:22:28 -0300

    fix graphql, mostrando el detalle de la propiedad

--- src/components/properties.js
+++ src/components/properties.js
@@ -1,18 +1,19 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+import Iconos from "./iconos"
 
 import Layout from "./layout"
 
 export const query = graphql`
-  query($id: String!) {
+  query ($id: String!) {
     allStrapiPropiedad(filter: { id: { eq: $id } }) {
       nodes {
         imagen {
           localFile {
-            sharp: childImageSharp {
-              fluid(maxWidth: 700) {
-                ...GatsbyImageSharpFluid_withWebp
-              }
+            childImageSharp {
+              gatsbyImageData(width: 1200)
             }
           }
         }
@@ -33,7 +34,6 @@ export const query = graphql`
         agente {
           email
           nombre
-          id
           telefono
         }
       }
@@ -41,12 +41,51 @@ export const query = graphql`
   }
 `
 
-const Properties = ({ data: { allStrapiPropiedad: { nodes } } }) => {
+const Properties = ({
+  data: {
+    allStrapiPropiedad: { nodes },
+  },
+}) => {
   console.log("🚀 ~ file: property.js ~ line 45 ~ Property ~ nodes", nodes[0])
+
+  const {
+    nombre,
+    habitaciones,
+    banhos,
+    precio,
+    estacionamiento,
+    imagen,
+    descripcion,
+    agente,
+  } = nodes[0]
 
   return (
     <Layout>
-      <h1>Properties</h1>
+      <h1>{nombre}</h1>
+      <div>
+        <main>
+          <GatsbyImage
+            image={imagen.localFile.childImageSharp.gatsbyImageData}
+            alt="imagen de propiedad"
+          />
+          <p>{descripcion.data.descripcion}</p>
+        </main>
+        <aside>
+          <p>${precio}</p>
+          <Iconos
+            banhos={banhos}
+            estacionamiento={estacionamiento}
+            habitaciones={habitaciones}
+          />
+
+          <div>
+            <h2>Vendedor: </h2>
+            <p>{agente.nombre}</p>
+            <p>Telefono: {agente.telefono}</p>
+            <p>Email: {agente.email}</p>
+          </div>
+        </aside>
+      </div>
     </Layout>
   )
 }
