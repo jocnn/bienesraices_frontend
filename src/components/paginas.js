8a3bfad614ea9f1808b83ac2dc950cca99cd27cc
jocnn <ijann_@outlook.com>
2022-07-01 07:36:05 -0300

    se creo y consulto con gql para exponer los datos

---
+++ src/components/paginas.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { graphql } from "gatsby"
+// import { GatsbyImage } from "gatsby-plugin-image"
+// import styled from "@emotion/styled"
+
+import Layout from "./layout"
+
+// const Contenido = styled.div`
+//   max-width: 120rem;
+//   margin: 0 auto;
+//   width: 95%;
+
+//   @media (min-width: 768px) {
+//     display: grid;
+//     grid-template-columns: 2fr 1fr;
+//     column-gap: 5rem;
+//   }
+// `
+
+export const query = graphql`
+  query ($id: String!) {
+    allStrapiPagina(filter: { id: { eq: $id } }) {
+      nodes {
+        titulo
+        contenido
+        imagen {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(width: 1200)
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Paginas = ({ data: { allStrapiPagina: { nodes } } }) => {
+  console.log("🚀 ~ file: pages.js ~ line 39 ~ Pages ~ nodes", nodes)
+
+  // const { titulo, contenido, imagen } = nodes[0]
+
+  return (
+    <Layout>
+      <main>
+        {/* <h1>{titulo}</h1>
+        <Contenido>
+          <GatsbyImage
+            image={imagen.localFile.childImageSharp.gatsbyImageData}
+            alt="imagen de propiedad"
+          />
+          <p>{contenido}</p>
+        </Contenido> */}
+      </main>
+    </Layout>
+  )
+}
+
+export default Paginas
