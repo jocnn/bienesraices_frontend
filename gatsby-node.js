8a16832824afbe661fef00c11e99e0682a61b9fc
jocnn <ijann_@outlook.com>
2022-07-01 07:33:09 -0300

    se creo la consulta para páginas y su acción

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,12 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           nombre
         }
       }
+      allStrapiPagina {
+        nodes {
+          id
+          titulo
+        }
+      }
     }
   `)
   // console.log("🚀 ~ file: gatsby-node.js ~ line 20 ~ exports.createPages= ~ result", JSON.stringify(result))
@@ -17,14 +23,27 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.panic("No hubo resultados", result.error)
   }
 
+  const paginas = result.data.allStrapiPagina.nodes
+  // console.log("🚀 ~ file: gatsby-node.js ~ line 27 ~ exports.createPages= ~ paginas", paginas)
+
   const propiedades = result.data.allStrapiPropiedad.nodes
-  // console.log("🚀 ~ file: gatsby-node.js ~ line 27 ~ exports.createPages= ~ propiedades", JSON.stringify(propiedades))
+  // console.log("🚀 ~ file: gatsby-node.js ~ line 30 ~ exports.createPages= ~ propiedades", JSON.stringify(propiedades))
+
+  paginas.forEach(pagina => {
+    actions.createPage({
+      path: urlSlug(pagina.titulo),
+      component: require.resolve("./src/components/paginas.js"),
+      context: {
+        id: pagina.id
+      }
+    })
+  })
 
   propiedades.forEach(propiedad => {
-    // console.log("🚀 ~ file: gatsby-node.js ~ line 30 ~ exports.createPages= ~ propiedad", propiedad.id)
+    // console.log("🚀 ~ file: gatsby-node.js ~ line 43 ~ exports.createPages= ~ propiedad", propiedad.id)
     actions.createPage({
       path: urlSlug(propiedad.nombre),
-      component: require.resolve('./src/components/properties.js'),
+      component: require.resolve('./src/components/propiedades.js'),
       context: {
         id: propiedad.id
       }
