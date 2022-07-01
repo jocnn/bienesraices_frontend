db37e97ade9afc227102117cca0c634bf4e9f2e9
jocnn <ijann_@outlook.com>
2022-07-01 08:03:32 -0300

    mostrando datos de la página dinámica generada

--- src/components/paginas.js
+++ src/components/paginas.js
@@ -1,21 +1,22 @@
 import React from "react"
 import { graphql } from "gatsby"
-// import { GatsbyImage } from "gatsby-plugin-image"
-// import styled from "@emotion/styled"
+import { GatsbyImage } from "gatsby-plugin-image"
+import styled from "@emotion/styled"
 
 import Layout from "./layout"
+import ListadoPropiedades from "./listProperties"
 
-// const Contenido = styled.div`
-//   max-width: 120rem;
-//   margin: 0 auto;
-//   width: 95%;
+const Contenido = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  width: 95%;
 
-//   @media (min-width: 768px) {
-//     display: grid;
-//     grid-template-columns: 2fr 1fr;
-//     column-gap: 5rem;
-//   }
-// `
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    column-gap: 5rem;
+  }
+`
 
 export const query = graphql`
   query ($id: String!) {
@@ -36,22 +37,27 @@ export const query = graphql`
 `
 
 const Paginas = ({ data: { allStrapiPagina: { nodes } } }) => {
-  console.log("🚀 ~ file: pages.js ~ line 39 ~ Pages ~ nodes", nodes)
+  // console.log("🚀 ~ file: pages.js ~ line 40 ~ Pages ~ nodes", nodes)
 
-  // const { titulo, contenido, imagen } = nodes[0]
+  const { titulo, contenido, imagen } = nodes[0]
 
   return (
     <Layout>
       <main>
-        {/* <h1>{titulo}</h1>
+        <h1>{titulo}</h1>
         <Contenido>
           <GatsbyImage
             image={imagen.localFile.childImageSharp.gatsbyImageData}
             alt="imagen de propiedad"
           />
           <p>{contenido}</p>
-        </Contenido> */}
+        </Contenido>
       </main>
+      {
+        ( titulo === "Propiedades" ) ? (
+          <ListadoPropiedades />
+        ) : null
+      }
     </Layout>
   )
 }
